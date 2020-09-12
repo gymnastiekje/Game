6b8e7bcdd8bda3dde6c58dc514698bc374490c28
Louise <70775180+grapjas@users.noreply.github.com>
2020-09-12 18:01:34 +0200

    Update Schaar_Steen_Papier.py
    Put a space after the # and indented the comments correctly

--- Schaar_Steen_Papier.py
+++ Schaar_Steen_Papier.py
@@ -2,12 +2,12 @@ import random
 
 class bcolors:
     ENDC='\033[0m'
-    BOLD='\033[01m' #Maakt vet
-    DISABLE='\033[02m' #Maakt lichter
-    UNDERLINE='\033[04m' #Onderlijnt
-    REVERSE='\033[07m' #Witte achtergrond
+    BOLD='\033[01m'         # vet
+    DISABLE='\033[02m'      # lichter
+    UNDERLINE='\033[04m'    # onderlijnt
+    REVERSE='\033[07m'      # witte achtergrond
     STRIKETHROUGH='\033[09m'
-    INVISIBLE='\033[08m'#Maamkt onzichtbaar, duhhh
+    INVISIBLE='\033[08m'    # onzichtbaar
 class text:   
     BLACK='\033[30m'
     RED='\033[31m'
@@ -45,46 +45,46 @@ def speler_input():
 
 
 def computer_input():
-    computer_keuze = keuzes[random.randint(0,2)]#kiest element uit lijst; 0, 1 of 2
+    computer_keuze = keuzes[random.randint(0,2)]    # kiest element uit lijst; 0, 1 of 2
     return computer_keuze
 
 def who_wins():
     winnaar = 0
-    while (winnaar == 0): #while zorgt ervoor dat het blijft verder gaan als winnaar = 0
+    while (winnaar == 0):   # while zorgt ervoor dat het blijft verder gaan als winnaar = 0
         computer = computer_input()
         speler = speler_input()
         if speler == computer:
             winnaar = 0
             print("De computer had ook", computer,", opnieuw!")
             continue
-        winnaar = 1 #de volgende if/elif zorgt ervoor dat als jij zou winnen winnaar = 2, anders blijft winnaar 1
+        winnaar = 1 # de volgende if/elif zorgt ervoor dat als jij zou winnen winnaar = 2, anders blijft winnaar 1
         if speler == "Papier" and computer == "Steen":
             winnaar = 2
         elif speler == "Schaar" and computer =="Papier":
             winnaar = 2
         elif speler == "Steen" and computer == "Schaar":
             winnaar = 2
-    return(winnaar, speler, computer) #Zorgen dat de if/elif statemets in de loop staan
-#geen [] bij return als dat niet ndg is. [] duid een lijst aan; () of niets een tupel. 
-#lijst = kan aangepast worden; tupel = kan niet aangepast worden
+    return(winnaar, speler, computer) # zorgen dat de if/elif statemets in de loop staan
+# geen [] bij return als dat niet ndg is. [] duid een lijst aan; () of niets een tupel. 
+# lijst = kan aangepast worden; tupel = kan niet aangepast worden
 
 
 
 
-#Werking: Dictionary = {"key":value
+# werking: Dictionary = {"key":value
 #                       "key":value
 #                        etc.}
-#dictionary[key]=value
+# dictionary[key]=value
 
-#emojis["papier"] geeft \U...
+# emojis["papier"] geeft \U...
 emojis ={ "Schaar": "\U0001F596",
          "Steen": "\U0001F44A",
          "Papier": "\U0001F91A"}
-#avatar["Kakje"] geeft \U...
+# avatar["Kakje"] geeft \U...
 avatar ={"Kakje":"\U0001F4A9",
           "Raket":"\U0001F680",
           "Smiley":"\U0001F600"}
-#cavatar["Kakje"] geeft text.
+# avatar["Kakje"] geeft text.
 cavatar ={"Kakje": text.ORANGE,
           "Raket": text.CYAN,
           "Smiley": text.YELLOW}
@@ -112,9 +112,9 @@ print(text.YELLOW,"Nu kunnen we beginnen.\n"
 score_speler = 0
 score_computer = 0
 
-while(1):#while(x) herhaalt enkel als uw x niet gelijk is aan 0
-    winnaar, speler, computer = who_wins() #volgorde is belangrijk! Hier heb je who_wins() die [winnaar, speler, computer] bevat. 
-    if winnaar == 1:                       #Daarna stel ik dat winnaar=winnnaar, speler=speler, computer=computer; als volgorde verschillend is stel ik bv. computer=speler
+while(1):   # while(x) herhaalt enkel als uw x niet gelijk is aan 0
+    winnaar, speler, computer = who_wins() # volgorde is belangrijk! Hier heb je who_wins() die [winnaar, speler, computer] bevat. 
+    if winnaar == 1:                       # Daarna stel ik dat winnaar=winnnaar, speler=speler, computer=computer; als volgorde verschillend is stel ik bv. computer=speler
         print(emojis[speler],"verliest tegen", emojis[computer])
         score_computer += 1
     elif winnaar == 2:
@@ -131,10 +131,10 @@ while(1):#while(x) herhaalt enkel als uw x niet gelijk is aan 0
 
     
 
-#schaar verliest steen
-#steen verliest papier
-#papier verliest schaar
+# schaar verliest steen
+# steen verliest papier
+# papier verliest schaar
 
-#gelijk = 0
-#computer gewonnen = 1
-#speler gewonnen = 2
+# gelijk = 0
+# computer gewonnen = 1
+# speler gewonnen = 2
